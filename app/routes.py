cc2c14a15580a967e902aa9b0d9a63bfc83d17ff
erin nicole johnson <erin.nicole.johnson@gmail.com>
2020-04-29 20:32:03 +0100

    install WTFflaskforms, add login and route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'erin'}
+    return render_template('index.html', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('login requeted for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='sign in', form=form)
