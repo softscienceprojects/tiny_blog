41128d01a6d3e59f8f4c20c57b2438525cbe8a1e
erin nicole johnson <erin.nicole.johnson@gmail.com>
2020-05-04 20:02:19 +0100

    add profile page with gravatar

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -17,6 +18,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=retro&s={}'.format(digest, size)
 
 @login.user_loader
 def load_user(id):
