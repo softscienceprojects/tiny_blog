4b50287e7384252c3df204115c60a516b0f98ec8
erin nicole johnson <erin.nicole.johnson@gmail.com>
2020-05-05 20:20:17 +0100

    error handling

--- app/forms.py
+++ app/forms.py
@@ -31,3 +31,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     about_me = TextAreaField('about me', validators=[Length(min=0, max=140)])
     submit = SubmitField('submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs) # super - inherit/extend the functionality from FlaskForm, 
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
