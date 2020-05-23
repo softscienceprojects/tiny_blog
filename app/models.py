c3e921b28408e7ff48a838e9b969e1517926122d
erin nicole johnson <erin.nicole.johnson@gmail.com>
2020-05-23 15:23:22 +0100

    install HTTPie

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class PaginatedAPIMixin(object):
     def to_collection_dict(query, page, per_page, endpoint, **kwargs): #first three SQLAlchemy query objects
         resources = query.paginate(page, per_page, False)
         data = {
-            'items': [item.to_dict(), for item in resources.items],
+            'items': [item.to_dict() for item in resources.items],
             '_meta': {
                 'page': page,
                 'per_page': per_page,
@@ -33,7 +33,7 @@ class PaginatedAPIMixin(object):
                 'prev': url_for(endpoint, page=page-1, per_page=per_page, **kwargs) if resources.has_prev else None
             }
         }
-    return data
+        return data
 
 
 class User(PaginatedAPIMixin, UserMixin, db.Model):
